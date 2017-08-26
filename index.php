6c9488db2fefe0b7856410546ce7f3984796802e
jhubz <jhubz@list.ru>
2017-08-26 11:40:08 +0300

    Добавил 2 пробела для однообразности кода

--- index.php
+++ index.php
@@ -161,26 +161,26 @@ $lots = [
             </select>
         </div>
         <ul class="lots__list">
-          
+
             <?php foreach ($lots as $lot): ?>
-              <li class="lots__item lot">
-                  <div class="lot__image">
-                      <img src="<?=$lot["img"];?>" width="350" height="260" alt="Сноуборд">
-                  </div>
-                  <div class="lot__info">
-                      <span class="lot__category"><?=$lot["category"];?></span>
-                      <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot["title"];?></a></h3>
-                      <div class="lot__state">
-                          <div class="lot__rate">
-                              <span class="lot__amount">Стартовая цена</span>
-                              <span class="lot__cost"><?=$lot["price"];?><b class="rub">р</b></span>
-                          </div>
-                          <div class="lot__timer timer">
-                              <?=$lot_time_remaining;?>
-                          </div>
-                      </div>
-                  </div>
-              </li>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$lot["img"];?>" width="350" height="260" alt="Сноуборд">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$lot["category"];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot["title"];?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$lot["price"];?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                <?=$lot_time_remaining;?>
+                            </div>
+                        </div>
+                    </div>
+                </li>
             <?php endforeach; ?>
 
         </ul>
