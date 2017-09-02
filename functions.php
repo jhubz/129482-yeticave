054ac438554bdc66ce8a196cb030b17d2255d3a5
jhubz <jhubz@list.ru>
2017-09-02 10:14:38 +0300

    Добавил extract в функцию render_template

--- functions.php
+++ functions.php
@@ -3,9 +3,11 @@
     if (!file_exists($path)) {
       return '';
     }
-    
+
+    extract($data);
+
     ob_start();
     require $path;
-    
+
     return ob_get_clean();
   }
--- templates/lot_item.php
+++ templates/lot_item.php
@@ -1,17 +1,17 @@
 <li class="lots__item lot">
     <div class="lot__image">
-        <img src="<?=$data['lot']['img'];?>" width="350" height="260" alt="Сноуборд">
+        <img src="<?=$lot['img'];?>" width="350" height="260" alt="Сноуборд">
     </div>
     <div class="lot__info">
-        <span class="lot__category"><?=$data['lot']['category'];?></span>
-        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($data['lot']['title']);?></a></h3>
+        <span class="lot__category"><?=$lot['category'];?></span>
+        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($lot['title']);?></a></h3>
         <div class="lot__state">
             <div class="lot__rate">
                 <span class="lot__amount">Стартовая цена</span>
-                <span class="lot__cost"><?=htmlspecialchars($data['lot']['price']);?><b class="rub">р</b></span>
+                <span class="lot__cost"><?=htmlspecialchars($lot['price']);?><b class="rub">р</b></span>
             </div>
             <div class="lot__timer timer">
-                <?=$data['lot_time_remaining'];?>
+                <?=$lot_time_remaining;?>
             </div>
         </div>
     </div>
